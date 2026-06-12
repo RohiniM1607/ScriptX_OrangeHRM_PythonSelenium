c8386ff16085b9c54e60ca0ff68706bf1fb8a53f
RohiniM1607 <mrohini7558@gmail.com>
2026-06-12 07:51:13 +0530

    Rohini: CreateUserCredentials-Test

--- ScriptX_OrangeHRM/Tests/test_create_user_credentials.py
+++ ScriptX_OrangeHRM/Tests/test_create_user_credentials.py
@@ -7,8 +7,8 @@ from Utilities import log_creator
 
 @pytest.mark.usefixtures("setup_and_teardown")
 @pytest.mark.parametrize("role, emp_name, status, user_name, password, confirmpassword", Excel_Reader.get_data("Configurations/TestData.xlsx", "CreateUserCredentials"))
-class TestCreateUserCredentials:
-    def test_create_valid_user_credentials(self, role, emp_name, status, user_name, password, confirmpassword):
+class TestCreateAdminUserCredentials:
+    def test_create_admin_user_credentials(self, role, emp_name, status, user_name, password, confirmpassword):
         
         login_action = LoginAction(self.driver)
         login_action.login_with_valid_credentials(
@@ -19,9 +19,7 @@ class TestCreateUserCredentials:
         actions.click_admin_menu()
         actions.click_add_button()
         assert actions.verify_add_user_page(), "Add User page is not displayed."
-
         actions.enter_user_credentials(role,emp_name,status,user_name,password,confirmpassword)
-
         actions.click_save_button()
         assert actions.verify_success_message_displayed(), "Success message not displayed."
             
