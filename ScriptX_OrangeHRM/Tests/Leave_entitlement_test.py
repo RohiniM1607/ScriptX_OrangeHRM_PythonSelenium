e1aff62d1809d180506dc357979c72e9d5c3de9d
Mylambigai <mylambigai16@gmail.com>
2026-06-15 08:31:46 +0530

    Added scenario

--- ScriptX_OrangeHRM/Tests/Leave_entitlement_test.py
+++ ScriptX_OrangeHRM/Tests/Leave_entitlement_test.py
@@ -4,13 +4,14 @@ from Actions.login_action import LoginAction
 from Actions.Leave_entitlement_action import AddLeaveEntitlementActions
 from Utilities import Excel_Reader
 
-
+@pytest.mark.leaveEntitlement
 @pytest.mark.usefixtures("setup_and_teardown")
 class TestLeaveEntitlement:
     
+    @pytest.mark.myl
     @pytest.mark.parametrize("EmployeeName, LeaveType, Entitlement",
-            [("Emily Jones","CAN - Personal", "50"),
-            ("Employee 1","CAN - Personal", "60")])
+            [("Ravi M B","CAN - Personal", "50"),
+            ("Ravi M B","CAN - Vacation", "60")])
     def test_valid_leave_entitlement(self,EmployeeName,LeaveType,Entitlement):
 
         LoginAction(self.driver).login(
@@ -30,7 +31,7 @@ class TestLeaveEntitlement:
         action.save()
         action.confirm_msg()
 
-
+    @pytest.mark.myl
     @pytest.mark.parametrize("LeaveType, Entitlement", Excel_Reader.invalid_AddLeave_data("Configurations/TestData.xlsx", "All Leave"))
     def test_required_validation(self,LeaveType,Entitlement):
 
@@ -48,6 +49,7 @@ class TestLeaveEntitlement:
 
         assert action.is_required_msg_displayed()
   
+    @pytest.mark.myl
     @pytest.mark.parametrize("EmployeeName, LeaveType, Entitlement", Excel_Reader.exceed_LeaveLimit("Configurations/TestData.xlsx", "All Leave"))
     def test_exceed_validation(self,EmployeeName,LeaveType,Entitlement):
 
@@ -65,3 +67,20 @@ class TestLeaveEntitlement:
         action.enter_entitlement(Entitlement)
 
         assert action.is_exceed_error_displayed()
+
+    @pytest.mark.myl
+    @pytest.mark.parametrize("EmployeeName", ["Myl"])
+    def test_invalid_employeeName(self, EmployeeName):
+
+        LoginAction(self.driver).login(
+        get_config("Login Details", "username"),
+        get_config("Login Details", "password"))
+
+        action = AddLeaveEntitlementActions(self.driver)
+
+        action.open_leave_page()
+        action.enter_employee(EmployeeName)
+
+        no_record_msg = action.invalid_empName()
+
+        assert no_record_msg == "No Records Found"
