047fb45d8f3f5175a327ca498a366028af0ec5b9
Mylambigai <mylambigai16@gmail.com>
2026-08-21 18:38:02 +0530

    Updation

--- ScriptX_OrangeHRM/Tests/Leave_entitlement_test.py
+++ ScriptX_OrangeHRM/Tests/Leave_entitlement_test.py
@@ -32,7 +32,7 @@ class TestLeaveEntitlement:
         action.confirm_msg()
 
     @pytest.mark.myl
-    @pytest.mark.parametrize("LeaveType, Entitlement", Excel_Reader.invalid_AddLeave_data("Configurations/TestData.xlsx", "All Leave"))
+    @pytest.mark.parametrize("LeaveType, Entitlement", Excel_Reader.invalid_AddLeave_data("ScriptX_OrangeHRM/Configurations/TestData.xlsx", "All Leave"))
     def test_required_validation(self,LeaveType,Entitlement):
 
         LoginAction(self.driver).login(
@@ -50,7 +50,7 @@ class TestLeaveEntitlement:
         assert action.is_required_msg_displayed()
   
     @pytest.mark.myl
-    @pytest.mark.parametrize("EmployeeName, LeaveType, Entitlement", Excel_Reader.exceed_LeaveLimit("Configurations/TestData.xlsx", "All Leave"))
+    @pytest.mark.parametrize("EmployeeName, LeaveType, Entitlement", Excel_Reader.exceed_LeaveLimit("ScriptX_OrangeHRM/Configurations/TestData.xlsx", "All Leave"))
     def test_exceed_validation(self,EmployeeName,LeaveType,Entitlement):
 
         LoginAction(self.driver).login(
